0da2fe53377568e6c9d1a11d845413c4b70748bf
Naushad Ali <114598077+naushadali1@users.noreply.github.com>
2023-01-18 17:39:40 +0500

    Add Comments
    Add comments to improve readability

--- Variable.java
+++ Variable.java
@@ -4,9 +4,9 @@ import java.util.*;
 public class Variable {
     public static void main(String[] args) {
       System.out.println("Enter first number");
-      Scanner sc= new Scanner(System.in);
+      Scanner sc= new Scanner(System.in); // create sc object of class Scanner which use for input
 
-     int num1 = sc.nextInt();
+     int num1 = sc.nextInt(); // num1 is first variable that take input integer value from user
       System.out.println("Enter SEcond number");
       int num2 =sc.nextInt();
       int sum =num1+num2;
