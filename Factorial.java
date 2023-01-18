13364ad28c07dfda596b00ac0268e619f6d8fcc9
Naushad Ali <nashadali585@gmail.com>
2023-01-18 18:11:07 +0500

    creatae Factorial Program
    create a factorial program through pc file

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int fact = 1;
+        int number = 5;
+        for (int index = 1; index < number; index++) {
+            fact = fact * index;
+        }
+        System.out.println("Factorial of " + number + " is: " + fact);
+    }
+}
