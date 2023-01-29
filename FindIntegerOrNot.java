32c9a49de0c26490ab2944f919902d6966c1a29d
Naushad Ali <nashadali585@gmail.com>
2023-01-29 21:21:50 +0500

    find integer number

--- FindIntegerOrNot.java
+++ FindIntegerOrNot.java
@@ -2,7 +2,15 @@ import java.util.Scanner;
 
 public class FindIntegerOrNot {
     public static void main(String[] args) {
-        
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter number : ");
+      try {
+        int number = Integer.parseInt(sc.nextLine());
+        System.out.println(number + " is An Integer");
+      } catch (NumberFormatException e) {
+        System.out.println(e + " is not an Integer");
+      }
+       sc.close();
     }
         }
     
