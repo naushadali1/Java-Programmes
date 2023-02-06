05767d719df6c19061a2347718bd7ebd1ddf0785
Naushad Ali <nashadali585@gmail.com>
2023-02-06 22:24:36 +0500

    Even Number

---
+++ EvenNumber.java
@@ -0,0 +1,7 @@
+public class EvenNumber {
+    public static void main(String[] args) {
+        for (int index = 0; index <= 100; index++) {
+            System.out.println(index * 2);
+        }
+    }
+}
