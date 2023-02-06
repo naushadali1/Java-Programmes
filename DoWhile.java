f0823e2840768997f80d50f450eadd96772d4ab8
Naushad Ali <nashadali585@gmail.com>
2023-02-06 22:07:55 +0500

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,8 @@
+class DoWhile {
+    public static void main(String[] args) {
+        int a = 10;
+        do {
+            System.out.println(a + " I m Do While loop");
+        } while (a <= 2);
+    }
+}
